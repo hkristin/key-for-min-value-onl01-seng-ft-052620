3e9bb7e9ad15b4b6538e3d486c501cd539522f95
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-02 16:24:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
    if lowest_value == nil || value < lowest_value 
       lowest_value = value 
-      return_key = key
+      lowest_key = key
     binding.pry
   end
     return return_key
